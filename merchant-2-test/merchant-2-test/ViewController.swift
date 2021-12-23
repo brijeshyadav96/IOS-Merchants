8113a6bf870361ee6c45466c687cbae3acd96127
brijeshyadav96 <byadav@springbig.com>
2021-12-23 01:16:18 -0500

    First commit

---
+++ merchant-2-test/merchant-2-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  merchant-2-test
+//
+//  Created by Brijesh Yadav on 12/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
